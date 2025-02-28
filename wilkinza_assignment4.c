ae804e3ba342ba48a136f25c7185dd7b7482904d
CodePapayas <catspianos@gmail.com>
2025-02-28 11:54:38 -0800

    tokenize user input

--- wilkinza_assignment4.c
+++ wilkinza_assignment4.c
@@ -18,9 +18,31 @@ void get_input() {
     // Get user input
     printf(": ");
     fflush(stdout);
-    getline(&user_input, &buffer, stdin);
+    size_t input_ln = getline(&user_input, &buffer, stdin);
+
+    if (input_ln > 2048 || input_ln < 0) {
+        printf("Error: Invalid input\n");
+        fflush(stdout);
+            exit(1);
+        }
+
+    // Tokenize user input
+    char *token = strtok_r(user_input, " \n", &user_input);
+    int i = 0;
+    while (token != NULL) {
+        token_arr[i] = token;
+        i++;
+        token = strtok_r(NULL, " \n", &user_input);
+    }
+    token_arr[i] = NULL;
+
     puts(user_input);
     fflush(stdout);
+
+    for (int j = 0; j < i; j++) {
+        printf("token_arr[%d]: %s\n", j, token_arr[j]);
+        fflush(stdout);
+    }
 }
 
 int main() {
