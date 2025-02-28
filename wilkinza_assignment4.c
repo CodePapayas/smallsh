062fb22889f47a319ab53649aefe9bec109a5dc1
CodePapayas <catspianos@gmail.com>
2025-02-28 12:02:54 -0800

    Add background flag; Refactor tokenizing logic into it's own function

--- wilkinza_assignment4.c
+++ wilkinza_assignment4.c
@@ -5,28 +5,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <signal.h> 
+#include <signal.h>
+#include <stdbool.h>
 
 
 // Global Variables
 char *user_input = NULL; // User input
 size_t buffer = 0; // Buffer size
 char *token_arr[512]; // Arguments
+bool background_mode = false; // Background process flag
 
+// Get user input
 // Adapted from https://opensource.com/article/22/5/safely-read-user-input-getline
-void get_input() {
-    // Get user input
-    printf(": ");
-    fflush(stdout);
-    size_t input_ln = getline(&user_input, &buffer, stdin);
-
-    if (input_ln > 2048 || input_ln < 0) {
-        printf("Error: Invalid input\n");
-        fflush(stdout);
-            exit(1);
-        }
-
-    // Tokenize user input
+void tokenize_input() {
     char *token = strtok_r(user_input, " \n", &user_input);
     int i = 0;
     while (token != NULL) {
@@ -36,14 +27,29 @@ void get_input() {
     }
     token_arr[i] = NULL;
 
-    puts(user_input);
-    fflush(stdout);
-
     for (int j = 0; j < i; j++) {
         printf("token_arr[%d]: %s\n", j, token_arr[j]);
         fflush(stdout);
     }
 }
+
+void get_input() {
+    printf(": ");
+    fflush(stdout);
+    size_t input_ln = getline(&user_input, &buffer, stdin);
+
+    if (input_ln > 2048 || input_ln < 0) {
+        printf("Error: Invalid input\n");
+        fflush(stdout);
+        exit(1);
+    }
+
+    tokenize_input();
+
+    puts(user_input);
+    fflush(stdout);
+}
+
 
 int main() {
     // Get user input
