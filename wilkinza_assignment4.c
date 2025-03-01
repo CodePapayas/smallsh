aa515cd2b516dd960f2e2fd41602784398a42540
CodePapayas <catspianos@gmail.com>
2025-03-01 11:41:44 -0800

    Implement external command execution; refine background process management

--- wilkinza_assignment4.c
+++ wilkinza_assignment4.c
@@ -11,9 +11,11 @@
 
 // Global Variables
 char *user_input = NULL; // User input
-size_t buffer = 0; // Buffer size
 char *token_arr[512]; // Arguments
+pid_t bg_pids_arr[512]; // Array of background process IDs
+size_t buffer = 0; // Buffer size
 bool background_mode = false; // Background process flag
+int bg_pid_count = 0; // Number of background processes
 int i = 0; // Counter
 int last_exit_status = 0; // Exit status of last foreground process
 
@@ -57,6 +59,7 @@ bool empty_or_comment(char *tokens[]) {
     return true;
 }
 
+// Check for built-in commands
 bool check_built_in(char *tokens[]) {
     if (strcmp(tokens[0], "exit") == 0) {
         exit(0);
@@ -74,6 +77,41 @@ bool check_built_in(char *tokens[]) {
     }
     return false;
 }
+
+void check_external(char *tokens[]) {
+    pid_t pid = fork();
+    int status;
+
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+    else if (pid == 0) { // Child process
+        if (execvp(tokens[0], tokens) == -1) {
+            perror("execvp");
+            exit(1);
+        }
+    }
+    else { // Parent process
+        if (!background_mode) { // Foreground execution
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status)) {
+                last_exit_status = WEXITSTATUS(status);
+            }
+            else if (WIFSIGNALED(status)) {
+                last_exit_status = WTERMSIG(status);
+                printf("Process %d terminated by signal %d\n", pid, last_exit_status);
+            }
+        }
+        else { // Background execution
+                bg_pids_arr[bg_pid_count] = pid;
+                bg_pid_count++;
+                printf("Background process started: PID %d\n", pid);
+                fflush(stdout);
+        }
+    }
+}
+
 
 void get_input() {
     printf(": ");
@@ -106,13 +144,10 @@ int main() {
         get_input();
     } while (!empty_or_comment(token_arr));
     if (!check_built_in(token_arr)) {
-    check_background(token_arr);
-    if (background_mode) {
-        printf("Background mode enabled\n");
-        fflush(stdout);
+        check_background(token_arr);
+        check_external(token_arr);
     }
     print_input(token_arr);
     fflush(stdout);
-}
     return 0;
 }
