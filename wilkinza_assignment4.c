73942e7e78e1fc7f668fe24b9f7ab0750eaa125f
CodePapayas <catspianos@gmail.com>
2025-02-28 19:01:28 -0800

    Refactor functions to return values; add check for built in functions, blank lines, and comments

--- wilkinza_assignment4.c
+++ wilkinza_assignment4.c
@@ -14,10 +14,11 @@ char *user_input = NULL; // User input
 size_t buffer = 0; // Buffer size
 char *token_arr[512]; // Arguments
 bool background_mode = false; // Background process flag
+int i = 0; // Counter
 
 // Get user input
 // Adapted from https://opensource.com/article/22/5/safely-read-user-input-getline
-void tokenize_input() {
+void tokenize_input(char *tokens[]) {
     char *token = strtok_r(user_input, " \n", &user_input);
     int i = 0;
     while (token != NULL) {
@@ -26,11 +27,17 @@ void tokenize_input() {
         token = strtok_r(NULL, " \n", &user_input);
     }
     token_arr[i] = NULL;
+    };
 
-    for (int j = 0; j < i; j++) {
-        printf("token_arr[%d]: %s\n", j, token_arr[j]);
-        fflush(stdout);
+// Check for empty or comment lines, continue to prompt user for input
+bool empty_or_comment(char *tokens[]) {
+    if (tokens[0] == NULL || tokens[0][0] == '#') {
+        return false;
     }
+    else if (strspn(tokens[0], " \t") == strlen(tokens[0])) {
+        return false;
+    }
+    return true;
 }
 
 void get_input() {
@@ -44,15 +51,26 @@ void get_input() {
         exit(1);
     }
 
-    tokenize_input();
+    tokenize_input(token_arr);
+}
 
-    puts(user_input);
+void print_input(char *tokens[]) {
+    int i = 0;
+    while (tokens[i] != NULL) {
+        printf("%s ", tokens[i]);
+        fflush(stdout);
+        i++;
+    }
+    printf("\n");
     fflush(stdout);
 }
 
 
 int main() {
-    // Get user input
-    get_input();
+    do {
+        get_input();
+    } while (!empty_or_comment(token_arr));
+    print_input(token_arr);
+    fflush(stdout);
     return 0;
 }
