fe7cf7fd4836d9bd37ef9203cc1ef6891da4f45f
CodePapayas <catspianos@gmail.com>
2025-03-01 17:40:40 -0800

    Implement input/output redirection and enhance cleanup of background processes

--- wilkinza_assignment4.c
+++ wilkinza_assignment4.c
@@ -59,9 +59,73 @@ bool empty_or_comment(char *tokens[]) {
     return true;
 }
 
+// Check for redirection
+// Adapted from Exploration: Processes and I/O CS374
+void check_redirect(char *tokens[]) {
+    int input_fd = -1, output_fd = -1;
+    int i = 0;
+
+    while (tokens[i] != NULL) {
+        if (strcmp(tokens[i], "<") == 0) {
+            if (tokens[i + 1] == NULL) {
+                printf("Error: No input file specified\n");
+                last_exit_status = 1;
+                return;
+            }
+            input_fd = open(tokens[i + 1], O_RDONLY);
+            if (input_fd == -1) {
+                perror("source open()");
+                last_exit_status = 1;
+                return;
+            }
+            if (dup2(input_fd, 0) == -1) {
+                perror("source dup2()");
+                last_exit_status = 1;
+                return;
+            }
+            close(input_fd);
+
+            for (int j = i; tokens[j] != NULL; j++) {
+                tokens[j] = tokens[j + 2];
+            }
+            i--;
+        }
+        else if (strcmp(tokens[i], ">") == 0) {
+            if (tokens[i + 1] == NULL) {
+                printf("Error: No output file specified\n");
+                last_exit_status = 1;
+                return;
+            }
+            output_fd = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (output_fd == -1) {
+                perror("target open()");
+                last_exit_status = 1;
+                return;
+            }
+            if (dup2(output_fd, 1) == -1) {
+                perror("target dup2()");
+                last_exit_status = 1;
+                return;
+            }
+            close(output_fd);
+
+            for (int j = i; tokens[j] != NULL; j++) {
+                tokens[j] = tokens[j + 2];
+            }
+            i--;
+        }
+        i++;
+    }
+}
+
+
 // Check for built-in commands
 bool check_built_in(char *tokens[]) {
     if (strcmp(tokens[0], "exit") == 0) {
+        while (bg_pid_count > 0) {
+            kill(bg_pids_arr[bg_pid_count - 1], SIGKILL);
+            bg_pid_count--;
+        }
         printf("Exiting shell\n");
         fflush(stdout);
         exit(0);
@@ -90,8 +154,10 @@ void check_external(char *tokens[]) {
         exit(1);
     }
     else if (pid == 0) { // Child process
+        check_redirect(token_arr);
         if (execvp(tokens[0], tokens) == -1) {
             perror("execvp");
+            last_exit_status = 1;
             exit(1);
         }
     }
@@ -99,11 +165,11 @@ void check_external(char *tokens[]) {
         if (!background_mode) { // Foreground execution
             waitpid(pid, &status, 0);
             if (WIFEXITED(status)) {
-                last_exit_status = WEXITSTATUS(status);
+                last_exit_status = 1;
             }
             else if (WIFSIGNALED(status)) {
                 last_exit_status = WTERMSIG(status);
-                printf("Process %d terminated by signal %d\n", pid, last_exit_status);
+                printf("Process %d terminated by signal %d\n", pid, WTERMSIG(status));
             }
         }
         else { // Background execution
@@ -115,7 +181,8 @@ void check_external(char *tokens[]) {
     }
 }
 
-// Adapeted from Exploration: Process API - Monitoring Child Processes CS374
+// Cleanup background processes
+// Adapted from Exploration: Process API - Monitoring Child Processes CS374
 void cleanup_background() {
     int i = 0;
     while (i < bg_pid_count) {
@@ -141,7 +208,7 @@ void cleanup_background() {
     }
 }
 
-
+// Get user input
 void get_input() {
     printf(": ");
     fflush(stdout);
@@ -156,6 +223,7 @@ void get_input() {
     tokenize_input(token_arr);
 }
 
+// Print user input
 void print_input(char *tokens[]) {
     int i = 0;
     while (tokens[i] != NULL) {
